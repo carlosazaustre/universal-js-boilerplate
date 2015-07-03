781efda8153951f3d98825cf00c475e4a12fdddb
Carlos Azaustre <cazaustre@gmail.com>
2015-07-03 18:33:46 +0200

    remove folder to start again

--- app/client.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-
-import Client from 'react-engine/lib/client'
-import routes from './routes.js'
-
-document.addEventListener('DOMContentLoaded', () => {
-  Client.boot({ routes })
-})
