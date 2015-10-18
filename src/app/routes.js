a095a5d7554df44881c4ccd5f1ffada6437b719f
David Sancho <dsnxmoreno@gmail.com>
2015-10-19 00:10:25 +0200

    Comment routes for correct development

--- src/app/routes.js
+++ src/app/routes.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import { Route, DefaultRoute } from 'react-router';
-
-import App from './containers/App';
-import * as containers from './containers';
-
-const {
-  CounterPage,
-  About
-} = containers;
-
-const routes = (
-  <Route path='/' handler={ App }>
-    <DefaultRoute handler={ CounterPage }/>
-    <Route path='/components' handler={ CounterPage }/>
-    <Route path='/about' handler={ About }/>
-  </Route>
-);
-
-export default routes;
+// import React from 'react';
+// import { Route, DefaultRoute } from 'react-router';
+//
+// import App from './containers/App';
+// import * as containers from './containers';
+//
+// const {
+//   CounterPage,
+//   About
+// } = containers;
+//
+// const routes = (
+//   <Route path='/' handler={ App }>
+//     <DefaultRoute handler={ CounterPage }/>
+//     <Route path='/components' handler={ CounterPage }/>
+//     <Route path='/about' handler={ About }/>
+//   </Route>
+// );
+//
+// export default routes;
